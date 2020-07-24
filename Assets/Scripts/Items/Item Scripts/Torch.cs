431ffb89a07bce7d1d5f0cffafd937165e8bf718
Andrew Ward <andrewward22499@gmail.com>
2020-07-24 16:18:15 -0500

    Torches now create a light when used and fade out as time goes on.

--- Assets/Scripts/Items/Item Scripts/Torch.cs
+++ Assets/Scripts/Items/Item Scripts/Torch.cs
@@ -3,10 +3,27 @@
 [CreateAssetMenu(fileName = "Torch", menuName = "Item/Torch")]
 public class Torch : ScriptableItem
 {
+    private GameObject torchLight;
+    public int maxRange;
+    public float torchDuration;
+    private TorchTimer timer;
+
     public override void Use()
     {
-        base.Use();
-
-        RemoveFromInventory();
+        //Finds the light and turns it on
+        torchLight = GameObject.FindGameObjectWithTag("Torch Light");
+        if (torchLight != null)
+        {
+            timer = torchLight.GetComponent<TorchTimer>();
+            //Removes one use of the item from inventory
+            base.Use();
+            RemoveFromInventory();
+            //Timer for the torch's duration
+            timer.Info(torchDuration, maxRange);
+        }
+        else
+        {
+            Debug.LogError("Failed to find \"Torch Light\"");
+        }
     }
 }
